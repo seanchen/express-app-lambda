f2997f1561fdfc3d7c846bc75b015773d27a73d5
Sean Chen <sean.chen@leocorn.com>
2020-07-26 12:11:36 -0400

    add a new end point

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ app.get('/', (req, res) => {
         res.sendFile(`${__dirname}/index.html`)
 })
 
+// try to server a static text file.
+app.get('/txt', (req, res) => {
+        res.sendFile(`${__dirname}/index.txt`)
+})
+
 // app.listen(3000) // <-- comment this line out from your app
 module.exports = app // export your app so aws-serverless-express can use it
