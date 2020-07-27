33e97fae6e74bbf6ac725edc9b23f8e8690b2196
Sean Chen <sean.chen@leocorn.com>
2020-07-27 10:38:08 -0400

    we need set the status before send the response

--- routes/download.js
+++ routes/download.js
@@ -5,9 +5,8 @@ module.exports = function(app) {
     app.get("/download", function(req, res) {
 
         // set content type.
-        res.contentType('text/plain');
-        res.send('some simple content', {
-            'Content-Disposition': 'attachment; filename=aname.txt'
-        });
-    }
+        res.set('Content-Type', 'text/plain');
+        res.set('Content-Disposition', 'attachment; filename=aname.txt');
+        res.status(200).send('some simple content!');
+    });
 };
