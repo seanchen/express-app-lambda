c2684f1335df49a81e778b236b84f1cfbe4ec63d
Sean Chen <sean.chen@leocorn.com>
2020-07-27 10:02:21 -0400

    more code re-organize

--- core/express.js
+++ core/express.js
@@ -4,22 +4,8 @@ const express = require('express');
 module.exports = function() {
 
     let app = express();
-    
-    app.get('/', (req, res) => {
 
-        res.sendFile(`${__dirname}/index.html`);
-    })
-    
-    // try to server a static text file.
-    app.get('/txt', (req, res) => {
-
-        res.sendFile(`${__dirname}/index.txt`);
-    })
-    
-    // try to server a static text file.
-    app.get('/m3u', (req, res) => {
-        res.sendFile(`${__dirname}/index.m3u`);
-    });
+    require("../routes")(app);
 
     return app;
-}
+};
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = function(app) {
+
+    app.get('/', (req, res) => {
+
+        res.sendFile(`${__dirname}/index.html`);
+    })
+
+    // try to server a static text file.
+    app.get('/txt', (req, res) => {
+
+        res.sendFile(`${__dirname}/index.txt`);
+    })
+
+    // try to server a static text file.
+    app.get('/m3u', (req, res) => {
+        res.sendFile(`${__dirname}/index.m3u`);
+    });
+};
