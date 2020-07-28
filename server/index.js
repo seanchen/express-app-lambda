2343c8b75ba58dd9184797c410e4ab1ba8e0157c
Sean Chen <sean.chen@leocorn.com>
2020-07-28 12:33:20 -0400

    move around location again

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const app = require("./express")();
+const app = require("../core/express")();
 
 // app.listen(3000) // <-- comment this line out from your app
 module.exports = app // export your app so aws-serverless-express can use it
